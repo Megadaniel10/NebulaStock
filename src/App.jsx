0bd7e7745eb4bc529f2aac661141ed1c5fabdc89
Megadaniel10 <132786903+Megadaniel10@users.noreply.github.com>
2026-09-02 22:10:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { io } from 'socket.io-client';
 
 const DISCORD_CLIENT_ID = "1544048974175019058";
 // METTI QUI IL LINK DEL TUO BACKEND SU RENDER:
-const BACKEND_URL = "https://lake-kilometer-overturn.ngrok-free.dev"; 
+const BACKEND_URL = "https://concluded-forgot-encoding-prisoner.trycloudflare.comv"; 
 
 let socket;
 
