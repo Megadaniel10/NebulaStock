656ab7cb1cb7267091d96b264196e85da98ee67c
Megadaniel10 <132786903+Megadaniel10@users.noreply.github.com>
2026-09-16 16:58:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { io } from 'socket.io-client';
 
 const DISCORD_CLIENT_ID = "1544048974175019058";
 // MODIFICA QUI IL LINK DEL TUO BACKEND QUANDO CAMBIA SU CLOUDFLARE
-const BACKEND_URL = "https://anatomy-organ-versus-cloth.trycloudflare.com"; 
+const BACKEND_URL = "https://calvin-taxes-configured-trips.trycloudflare.com"; 
 
 let socket;
 
