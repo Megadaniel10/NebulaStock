f7b3905b6fedd87c1dd8301b5aa4566498094b98
Megadaniel10 <132786903+Megadaniel10@users.noreply.github.com>
2026-09-17 14:18:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { io } from 'socket.io-client';
 
 const DISCORD_CLIENT_ID = "1544048974175019058";
 // MODIFICA QUI IL LINK DEL TUO BACKEND QUANDO CAMBIA SU CLOUDFLARE
-const BACKEND_URL = "https://litigation-insider-respondent-iii.trycloudflare.com"; 
+const BACKEND_URL = "https://asin-var-updated-activists.trycloudflare.com"; 
 
 let socket;
 
