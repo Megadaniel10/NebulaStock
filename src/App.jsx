eb104b6fffb4f6946664d7d549b56dbb0b7b3f46
Megadaniel10 <132786903+Megadaniel10@users.noreply.github.com>
2026-09-18 16:00:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { io } from 'socket.io-client';
 
 const DISCORD_CLIENT_ID = "1544048974175019058";
 // MODIFICA QUI IL LINK DEL TUO BACKEND QUANDO CAMBIA SU CLOUDFLARE
-const BACKEND_URL = "https://travelling-texture-bench-polymer.trycloudflare.com"; 
+const BACKEND_URL = "https://testament-psychiatry-consistently-minor.trycloudflare.com"; 
 
 let socket;
 
