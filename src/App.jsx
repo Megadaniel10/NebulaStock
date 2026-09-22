b108f22982b5b1b8555f6db5f429293f5de51aa0
Megadaniel10 <132786903+Megadaniel10@users.noreply.github.com>
2026-09-22 15:29:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import tottiImg from './totti.png';
 
 const DISCORD_CLIENT_ID = "1544048974175019058";
 // MODIFICA QUI IL LINK DEL TUO BACKEND QUANDO CAMBIA SU CLOUDFLARE
-const BACKEND_URL = "https://miracle-varieties-beyond-excitement.trycloudflare.com"; 
+const BACKEND_URL = "https://queensland-students-investigators-adds.trycloudflare.com"; 
 
 let socket;
 
